da08311eca8bdbb0793ac770b539fa9ac228bdcd
Daniel Gallego <dani.gaza6@gmail.com>
2024-11-10 13:55:46 +0100

    Delete mov video

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -9,11 +9,11 @@ const LANG = {
 };
 
 export const getI18N = ({
-	currentLocale = 'es',
+	currentLocale = LANG.SPANISH,
 }: {
 	currentLocale: string | undefined;
 }) => {
-	if (currentLocale === LANG.NORWEGIAN) return {...spanish, ...norwegian};
-	if (currentLocale === LANG.ENGLISH) return {...spanish, ...english};
+	if (currentLocale === LANG.NORWEGIAN) return norwegian;
+	if (currentLocale === LANG.ENGLISH) return english;
 	return spanish;
 };
